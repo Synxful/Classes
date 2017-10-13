ec7a78609cdfaa5c9d544eb1b64bfdf3fc425e98
Synxful <32744519+Synxful@users.noreply.github.com>
2017-10-13 10:42:51 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Car porsche = new Car();
+        Car mercedes = new Car();
+
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+
+    }
+}
